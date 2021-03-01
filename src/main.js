59a8e783c58abdc0fdf24f45eba7a5209003869c
jordan <jorrand@gmail.com>
2021-03-01 15:06:58 -0700

    update

--- src/main.js
+++ src/main.js
@@ -1,55 +1,47 @@
-const { app, BrowserWindow, ipcMain, autoUpdater } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
 
-const createWindow = () => {
-  // Create the browser window.
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
     },
   });
+  mainWindow.loadFile("./src/index.html");
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
 
 app.on("ready", () => {
   createWindow();
   mainWindow.once('ready-to-show', () => {
-    autoUpdater.checkForUpdates();
+    autoUpdater.checkForUpdatesAndNotify();
   });
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
+app.on("window-all-closed", function () {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on("activate", () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
+app.on("activate", function () {
+  if (mainWindow === null) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
-ipcMain.on("ping", (event, msg) => {
-  console.log(msg); // msg from web page
-  event.reply("pong", "hi web page"); // send to web page
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
 });
 
 ipcMain.on("check-for-update", (event, msg) => {
-  autoUpdater.checkForUpdates();
+  autoUpdater.checkForUpdatesAndNotify();
 });
 
 autoUpdater.on('update-available', () => {
@@ -62,8 +54,3 @@ ipcMain.on('restart_app', () => {
   autoUpdater.quitAndInstall();
 });
 
-function sendStatusToWindow(text) {
-  if (mainWindow) {
-    mainWindow.webContents.send("message", `${text}`);
-  }
-}
