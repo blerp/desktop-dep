48fbbc2e2563d2b33d7d4567367cf684417d4f61
jordan <jorrand@gmail.com>
2021-03-01 15:03:59 -0700

    updates

--- src/main.js
+++ src/main.js
@@ -1,88 +1,4 @@
-const {
-  app,
-  BrowserWindow,
-  autoUpdater,
-  dialog,
-  ipcMain,
-} = require("electron");
-const path = require("path");
-const electron = require("electron");
-const process = require("process");
-const console = require("console");
-
-///////////////////
-// Auto upadater //
-///////////////////
-autoUpdater.requestHeaders = { "PRIVATE-TOKEN": process.env.PRIVATE_TOKEN };
-autoUpdater.autoDownload = true;
-
-// https://example.com/<namespace>/<project>/-/jobs/artifacts/auto-updater/raw/<path_to_file>?job=<job_name>
-// https://gitlab.com/blerp/desktop/-/jobs/artifacts/auto-updater/browse?job=build_windows
-// process.platform
-//`https://gitlab.com/blerp/desktop/-/jobs/artifacts/auto-updater/raw/out/make/squirrel.windows/x64/BlerpDesktop-${app.getVersion()} Setup.exe?job=build_windows`
-// const feedUrl = `https://gitlab.com/api/v4/projects/23152299/packages/generic/BlerpDesktop/${app.getVersion()}/BlerpDesktop-${app.getVersion()}.dmg`;
-const feedUrl = `https://gitlab.com/blerp/desktop/-/releases/BlerpDesktop-${app.getVersion()}_Setup.exe`;
-autoUpdater.setFeedURL({
-  provider: "generic",
-  url: feedUrl,
-});
-
-autoUpdater.on("checking-for-update", function () {
-  sendStatusToWindow(`Checking for update... ${feedUrl}`);
-  console.log("checking for update");
-});
-
-// setInterval(() => {
-//   autoUpdater.checkForUpdates();
-// }, 10000);
-
-autoUpdater.on("update-available", function (info) {
-  sendStatusToWindow("Update available.");
-  console.log("update avaliable");
-});
-
-autoUpdater.on("update-not-available", function (info) {
-  sendStatusToWindow("Update not available.");
-  console.log("update not avaliable");
-});
-
-autoUpdater.on("error", function (err) {
-  console.log(err);
-  sendStatusToWindow(`Checking for update... ${err}`);
-});
-
-autoUpdater.on("download-progress", function (progressObj) {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message =
-    log_message + " - Downloaded " + parseInt(progressObj.percent) + "%";
-  log_message =
-    log_message +
-    " (" +
-    progressObj.transferred +
-    "/" +
-    progressObj.total +
-    ")";
-  sendStatusToWindow(log_message);
-  console.log(log_message);
-});
-
-autoUpdater.on("update-downloaded", function (info) {
-  sendStatusToWindow("Update downloaded; will install in 1 seconds");
-});
-
-autoUpdater.on("update-downloaded", function (info) {
-  setTimeout(function () {
-    autoUpdater.quitAndInstall();
-  }, 1000);
-});
-
-autoUpdater.checkForUpdates();
-
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require("electron-squirrel-startup")) {
-  // eslint-disable-line global-require
-  app.quit();
-}
+const { app, BrowserWindow, ipcMain, autoUpdater } = require("electron");
 
 let mainWindow;
 
@@ -96,12 +12,12 @@ const createWindow = () => {
     },
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+app.on("ready", () => {
+  createWindow();
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdates();
+  });
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -134,7 +50,16 @@ ipcMain.on("ping", (event, msg) => {
 
 ipcMain.on("check-for-update", (event, msg) => {
   autoUpdater.checkForUpdates();
-  event.reply("check-for-update-response", "checking");
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
 });
 
 function sendStatusToWindow(text) {
